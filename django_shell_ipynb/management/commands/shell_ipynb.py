58842df85d1b12e19b33b5bb5fe560559613a297
Charl Botha <cpbotha@vxlabs.com>
2014-06-23 15:12:03 +0200

    Fixed for IPython 1.0+ and tested with 2.1

--- django_shell_ipynb/management/commands/shell_ipynb.py
+++ django_shell_ipynb/management/commands/shell_ipynb.py
@@ -2,7 +2,6 @@ from django.core.management.base import NoArgsCommand
 import os
 import sys
 
-
 class Command(NoArgsCommand):
 
     help = "Runs a Python interactive interpreter using the IPython notebook."
@@ -10,50 +9,13 @@ class Command(NoArgsCommand):
 
     def ipython_notebook(self):
         # analogous to the calls in the django.core shell command
-        from IPython.frontend.html.notebook.notebookapp import NotebookApp
+        from IPython.html.notebookapp import NotebookApp
+
         app = NotebookApp.instance()
-        app.initialize()
-
-        # here sys.path contains, as the first element, the directory containing
-        # manage.py. However, in the kernels that are started for the actual
-        # notebooks, sys.path does not contain this project directory. looks
-        # like this has been fixed in IPython, and should be out AFTER 0.13.2:
-        # https://github.com/ipython/ipython/commit/463ab6b388436efdb8bb2f949817e94c74f51dee
-
-        # until that time, after 0.13.2 is released, we need the following
-        # os.environ modification before and restoration after app.start()
-
-        # get current PYTHONPATH
-        pps = os.environ.get('PYTHONPATH')
-
-        if pps is None:
-            # remember that there was no PYTHONPATH to start with
-            nopp = True
-            pps = ''
-
-        else:
-            nopp = False
-
-        # break up into components
-        ppl = pps.split(os.pathsep)
-        # path containing manage.py
-        project_path = os.path.abspath(os.path.dirname(sys.argv[0]))
-        print project_path
-        # add it
-        ppl.append(project_path)
-        # and set it
-        os.environ['PYTHONPATH'] = os.pathsep.join(ppl)
-
-        # start the notebook
+        # send it empty argv (default is None), else it picks up django manage
+        # params and exits with a "no such file or directory" error
+        app.initialize(argv=[])
         app.start()
-
-        # user has quit the notebook, we restore the saved PYTHONPATH
-        if nopp:
-            # there was none to start with, so we delete it
-            del os.environ['PYTHONPATH']
-
-        else:
-            os.environ['PYTHONPATH'] = pps
 
     def handle_noargs(self, **options):
         # workaround taken from django.core.management.commands.shell
