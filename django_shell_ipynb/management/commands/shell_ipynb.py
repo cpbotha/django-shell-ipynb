de7e0a03d8fb56b3d8139a097de34fd14caae39e
Olivier Cortès <oc@1flow.io>
2014-11-08 14:06:05 +0100

    Allow to pass any command line option (or none) to the IPynb application (eg. now “./manage.py shell_ipynb --NotebookApp.ip='*'” works as expected).

--- django_shell_ipynb/management/commands/shell_ipynb.py
+++ django_shell_ipynb/management/commands/shell_ipynb.py
@@ -1,30 +1,29 @@
-from django.core.management.base import NoArgsCommand
+
+from django.core.management.base import BaseCommand
+
 import os
 import sys
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
 
     help = "Runs a Python interactive interpreter using the IPython notebook."
     requires_model_validation = False
+
+    def run_from_argv(self, argv):
+        self._argv = argv
+        self.execute()
 
     def ipython_notebook(self):
         # analogous to the calls in the django.core shell command
         from IPython.html.notebookapp import NotebookApp
 
         app = NotebookApp.instance()
-        # send it empty argv (default is None), else it picks up django manage
-        # params and exits with a "no such file or directory" error
-        app.initialize(argv=[])
+
+        app.initialize(argv=self._argv[1:])
         app.start()
 
-    def handle_noargs(self, **options):
-        # workaround taken from django.core.management.commands.shell
-        # XXX: (Temporary) workaround for ticket #1796: force early loading of all
-        # models from installed apps.
+    def handle(self, *args, **options):
         from django.db.models.loading import get_models
         get_models()
 
         self.ipython_notebook()
-
-
-
